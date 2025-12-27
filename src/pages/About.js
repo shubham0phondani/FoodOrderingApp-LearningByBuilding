f7e42f971c048e296dea7d8d44b2ed9a47f05c95
Shubham <phondanishubham23@gmail.com>
2025-12-27 17:31:47 +0530

    Aboutus page updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,43 @@
-const About = () =>{
+import User from "../components/User";
+import UserClass from "../components/UserClass";
+// import React from "react";
+import { Component } from "react";
+
+class About extends Component {
+
+    constructor(props){
+        super(props);
+        // console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        // console.log("parent Component Did Mount ");
+    }
+
+
+    render(){
+    console.log("Parent Render");
     return(
         <div>
-            <h1>About Us</h1>
+            <h1>About Class Component</h1>
             <h2>Food Ordering App</h2>
+            {/* <User name={"Shubham Phondani (function)"}/> */}
+            <UserClass name={"Shubham Phondani (class)"}  location={"Dehradun (class)"}/>
+            {/* <UserClass name={"Akshay Saini (class)"}  location={"Hyderabad (class)"}/> */}
         </div>
-    )
+    );
+    }
 }
+
+// const About = () =>{
+//     return(
+//         <div>
+//             <h1>About Us</h1>
+//             <h2>Food Ordering App</h2>
+//             {/* <User name={"Shubham Phondani (function)"}/> */}
+//             <UserClass name={"Shubham Phondani (class)"}  location={"Dehradun (class)"}/>
+//         </div>
+//     );
+// };
 
 export default About;
